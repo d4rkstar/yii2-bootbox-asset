47f48dac140ee5646f58b1138ae8e77ed47891fe
d4rkstar <d4rkstar@gmail.com>
2017-11-08 07:00:26 +0100

    changed namespace

--- BootBoxAsset.php
+++ BootBoxAsset.php
@@ -2,16 +2,17 @@
 /**
  * Asset for bootbox plugin
  * @author Brusenskiy Dmitry <brussens@nativeweb.ru>
+ * @author Bruno Salzano <d4rkstar@gmail.com>
  * @since 0.1.0
  * @version 0.1.2
- * @link https://github.com/yiiassets/yii2-bootbox-asset
+ * @link https://github.com/d4rkstar/yii2-bootbox-asset
  * @link http://bootboxjs.com/
  * @copyright 2015 Brusenskiy Dmitry
  * @license http://opensource.org/licenses/MIT MIT
- * @package yiiassets\bootbox
+ * @package d4rkstar\bootbox
  */
 
-namespace yiiassets\bootbox;
+namespace d4rkstar\bootbox;
 
 
 use Yii;
